af94d1a07372a14b38ee5026977a6c1247d83cb6
Athul <65664185+Athul0491@users.noreply.github.com>
2021-11-06 17:23:56 +0530

    cleanup

--- src/pvp.py
+++ src/pvp.py
@@ -22,7 +22,7 @@ def scraper(name,attack,defence,hp,league):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")    
-    browser = webdriver.Chrome(options=chrome_options)
+    # browser = webdriver.Chrome(options=chrome_options)
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     # PATH = 'C:\Program Files (x86)\chromedriver.exe'
     url = "https://pogostat.com/"
