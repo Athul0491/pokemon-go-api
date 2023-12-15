f4747d5d4d98f0df46e50a8a582ec7a526afbc11
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2023-12-15 19:01:39 +0530

    feat: reorganised imports

--- api.py
+++ api.py
@@ -11,8 +11,6 @@ from src.raid import Raid
 app = Flask(__name__)
 api = Api(app)
 
-
-
 api.add_resource(GreatLeague,"/api/great-league")
 api.add_resource(MasterLeague,"/api/master-league")
 api.add_resource(UltraLeague,"/api/ultra-league")
@@ -20,8 +18,6 @@ api.add_resource(Wild,"/api/wild")
 api.add_resource(Pvp,"/api/pvp-iv")
 api.add_resource(Details,"/api/pvp-details")
 api.add_resource(Raid,"/api/raid")
-
-
 
 if __name__ == "__main__":
     app.run()
